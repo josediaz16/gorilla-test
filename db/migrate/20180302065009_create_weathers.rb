9bf68853fdae33d010f70c8e4ad4364047ff9f31
Jose Diaz <jldiaz1612@gmail.com>
2020-08-30 19:34:06 -0500

    create migrations

--- db/migrate/20180302065009_create_weathers.rb
+++ db/migrate/20180302065009_create_weathers.rb
@@ -1,7 +1,9 @@
 class CreateWeathers < ActiveRecord::Migration
   def change
     create_table :weathers do |t|
-
+      t.date :date, null: false
+      t.text :temperatures, null: false, array: true
+      t.references :location, null: false, foreign_key: true
     end
   end
 end
--- db/migrate/20180302065020_create_locations.rb
+++ db/migrate/20180302065020_create_locations.rb
@@ -1,7 +1,10 @@
 class CreateLocations < ActiveRecord::Migration
   def change
     create_table :locations do |t|
-
+      t.decimal :latitude, null: false, scale: 4, precision: 12
+      t.decimal :longitude, null: false, scale: 4, precision: 12
+      t.string  :city, null: false
+      t.string  :state, null: false
     end
   end
 end
